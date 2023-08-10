7588996fb621b99a88fb64f6ff99fb06d3c97ffe
hngovan <hngovan1999@gmail.com>
2023-08-10 14:27:09 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'prettier',
+    'eslint-config-prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-unused-vars': [1, { args: 'after-used', argsIgnorePattern: '^_' }],
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: true,
+        bracketSameLine: false,
+        endOfLine: 'auto',
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true,
+        arrowParens: 'avoid',
+        quoteProps: 'as-needed',
+        insertPragma: false
+      }
+    ]
+  }
+}
